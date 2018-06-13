311c8155a6ae05e7b4750f16ced708d84ea296a5
yurko <4au.4au.4au@gmail.com>
2018-06-13 16:59:35 +0300

    f

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,19 @@
-$(window).load(function(){
-	
-});
+"use strict";
+
+var myScript = function(){
+
+
+	$(window).load(function(){
+
+	});
+
+	$(document).ready(function(){
+		$('.slider_site__carousel').owlCarousel({
+			loop:true,
+			margin:10,
+			nav:true
+		})
+	});
+}
+
+myScript();
