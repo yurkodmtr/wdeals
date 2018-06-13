3e1fa0f435f379bb67205072e33ba168b40c930c
Dmitriev Yurko <4au.4au.4au@gmail.com>
2018-06-13 21:00:53 +0300

    PIZDAAAAAAAAAAAAA

--- js/myscript.js
+++ js/myscript.js
@@ -10,9 +10,16 @@ var myScript = function(){
 	$(document).ready(function(){
 		$('.slider_site__carousel').owlCarousel({
 			loop:true,
-			margin:10,
+			items:1,
+			margin:0,
 			nav:true
-		})
+		});
+		$('.slider_result').owlCarousel({
+			loop:true,
+			items:1,
+			margin:0,
+			nav:true
+		});
 	});
 }
 
